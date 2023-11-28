7b6fb78b1e610e0218af9fd45219880f1cb1cc96
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-11-29 02:14:14 +0500

    done

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ var quill = new Quill('#editor-container', {
             [{ 'direction': 'rtl' }],
             [{ 'size': ['small', false, 'large', 'huge'] }],
             [{ 'color': [] }, { 'background': [] }],
-            [{ 'custom-color': '#000000' }]
+            [{ 'custom-color': '#000000' }],
+            [{ 'shadow': '#000' }]
         ]
     }
 });
 var customColorButton = document.querySelector('.ql-custom-color');
 customColorButton.innerHTML = '<input type="color" id="customColorPicker">';
+
+var customShadow = document.querySelector('.ql-shadow');
+customShadow.innerHTML = '<button id="shadowBtn" class="ml-1"> Shadow </button>';
 
 document.getElementById('customColorPicker').addEventListener('input', function () {
     var customColor = this.value;
@@ -49,4 +53,30 @@ document.getElementById('downloadButton').addEventListener('click', function () 
 
         document.body.removeChild(a);
     });
+});
+
+
+var checkValue = 0;
+$('#shadowBtn').click(function(){
+    if(checkValue == 0){
+        $('#editor-container .ql-editor p').css('text-shadow', '2px 2px 4px rgba(0, 0, 0, 0.5)');
+    }
+    $('.shadowBox').toggleClass('showShadowBox');
+    checkValue = 1;
+    console.log(checkValue)
+})
+
+$('#shadowHorizontal, #shadowVertical, #shadowBlur, #shadowColor').on('input', function () {
+    var horizontalOffset = $('#shadowHorizontal').val() + 'px';
+    var verticalOffset = $('#shadowVertical').val() + 'px';
+    var blurRadius = $('#shadowBlur').val() + 'px';
+    var shadowColor = $('#shadowColor').val();
+
+    // Apply text shadow to the Quill editor content
+    $('#editor-container .ql-editor p').css('text-shadow', horizontalOffset + ' ' + verticalOffset + ' ' + blurRadius + ' ' + shadowColor);
+
+    // Display current values
+    $('#shadowHorizontalValue').text(horizontalOffset);
+    $('#shadowVerticalValue').text(verticalOffset);
+    $('#shadowBlurValue').text(blurRadius);
 });
